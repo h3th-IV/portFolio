7ba265fb419c70ecc310ae1332930c1b9ee0a44a
Samuel Bonu <samuelbonux10@gmail.com>
2023-03-01 15:11:11 +0100

    links target="_blank"

--- src/Links.jsx
+++ src/Links.jsx
@@ -12,17 +12,26 @@ function Links() {
         <h1 className="Links__header">Links</h1>
 
         <p>You can reach me using any of the following links.</p>
-        <a className="bodyNavLink" href="https://twitter.com/codeAmbivert">
+        <a
+          className="bodyNavLink"
+          href="https://twitter.com/codeAmbivert"
+          target="_blank"
+        >
           <span className="linkLines"></span>
           <span className="linkLinesText">Twitter</span>
         </a>
-        <a className="bodyNavLink" href="https://github.com/codeAmbivert">
+        <a
+          className="bodyNavLink"
+          href="https://github.com/codeAmbivert"
+          target="_blank"
+        >
           <span className="linkLines"></span>
           <span className="linkLinesText">Github</span>
         </a>
         <a
           className="bodyNavLink"
           href="https://www.linkedin.com/in/titilope-chisom-onifade-7372681b2/"
+          target="_blank"
         >
           <span className="linkLines"></span>
           <span className="linkLinesText">LinkedIn</span>
@@ -30,11 +39,16 @@ function Links() {
         <a
           className="bodyNavLink"
           href="https://docs.google.com/document/d/1FNvukv2D9fQC95fbYjf8lHgKM8fqO9fqgV2A2pcrSWg/edit?usp=sharing"
+          target="_blank"
         >
           <span className="linkLines"></span>
           <span className="linkLinesText">Resume</span>
         </a>
-        <a className="bodyNavLink" href="mailto:titilopechisom20@gmail.com">
+        <a
+          className="bodyNavLink"
+          href="mailto:titilopechisom20@gmail.com"
+          target="_blank"
+        >
           <span className="linkLines"></span>
           <span className="linkLinesText">Gmail</span>
         </a>
